5864d11cb4daa016c943063c7bdfbac5866cf66a
lpatronr <lpatronr@gmail.com>
2022-12-08 14:23:54 -0500

    fix: navbar position

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -7,43 +7,36 @@ const Navbar: FC = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <>
-      <nav className="flex flex-wrap items-center justify-between px-2 py-3  mb-3 fixed w-full">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <Link
-              className="text-xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-blue-700"
-              href="/"
-            >
-              Naranjo{" "}
-              <span className="text-yellow-400">Express</span>
-            </Link>
-            <button
-              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <i className="fas fa-bars"></i>
-            </button>
-          </div>
-
-          <div
-            className={cs("lg:flex flex-grow items-center", navbarOpen ? "flex" : "hidden")}
+    <nav className="sticky top-0 z-10 mb-3 w-full items-center justify-between px-2 py-3">
+      <div className="container mx-auto flex flex-wrap items-center justify-between px-4">
+        <div className="relative flex w-full justify-between lg:static lg:block lg:w-auto lg:justify-start">
+          <Link
+            className="mr-4 inline-block whitespace-nowrap py-2 text-xl font-bold uppercase leading-relaxed text-blue-700"
+            href="/"
           >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-              {PAGES.map((page) => (
-                <li className="ml-2 text-black text-xl" key={page.name}>
-                  <Link href={page.href}>
-                    {page.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
+            Naranjo <span className="text-yellow-400">Express</span>
+          </Link>
+          <button
+            className="block cursor-pointer rounded border border-solid border-transparent bg-transparent px-3 py-1 text-xl leading-none text-white outline-none focus:outline-none lg:hidden"
+            type="button"
+            onClick={() => setNavbarOpen(!navbarOpen)}
+          >
+            <i className="fas fa-bars"></i>
+          </button>
         </div>
-      </nav>
-    </>
+
+        <div className={cs("flex-grow items-center lg:flex", navbarOpen ? "flex" : "hidden")}>
+          <ul className="flex list-none flex-col lg:ml-auto lg:flex-row">
+            {PAGES.map((page) => (
+              <li className="ml-2 text-xl text-black" key={page.name}>
+                <Link href={page.href}>{page.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
-}
+};
 
 export default Navbar;
